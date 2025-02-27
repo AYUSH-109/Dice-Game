32ad17d35dfd65e55b9664e9035f7c05545adda1
AYUSH-109 <nagarayush090@gmail.com>
2025-02-27 21:25:01 +0530

    files added

---
+++ index.js
@@ -0,0 +1,29 @@
+var randomnumber1 = (Math.floor((Math.random())*6) + 1);
+
+var randomdiceimage1 = "dice" + randomnumber1 + ".png";
+
+var randomimagesource1 = "./images/" + randomdiceimage1;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src",randomimagesource1);
+
+var randomnumber2 = (Math.floor((Math.random())*6) + 1);
+
+var randomdiceimage2 = "dice" + randomnumber2 + ".png";
+
+var randomimagesource2 = "./images/" + randomdiceimage2;
+
+var image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src",randomimagesource2);
+
+if(randomnumber1 > randomnumber2){
+    document.querySelector("h1").innerHTML = "🚩 player 1 wins!";
+}
+else if(randomnumber2 > randomnumber1){
+    document.querySelector("h1").innerHTML = "🚩 player 2 wins!";
+}
+else{
+    document.querySelector("h1").innerHTML = "DRAW";
+}
